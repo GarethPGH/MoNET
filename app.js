113829975f88b0d227db34df44b35fe23834df5a
LikeClockwork <jack@likeclockworkdesign.com>
2016-05-20 17:45:55 -0500

    test

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var connection_string = 'mongodb://localhost/monet';
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_URL + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
 }
