d753c0ce6160038287967e21fcf25e4faa935b32
LikeClockwork <jack@likeclockworkdesign.com>
2016-05-22 18:54:32 -0400

    fixed fucking RSS thing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var parser = require('rssparser');
-var rssoptions = {};
+var parser = require('parse-rss');
 
 var Sample = require('../models/models.js').Sample;
 var Job = require('../models/models.js').Job;
@@ -27,9 +26,9 @@ var calibration = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  parser.parseURL('http://blog.projectmo.net/rss', rssoptions, function(err, out){
+  parser('http://blog.projectmo.net/rss', function(err, out){
     if (err) throw err;
-    var latest = out.items[0];
+    var latest = out[0];
     latest.summary = latest.summary.replace(/<(?:.|\n)*?>/gm, '').slice(0,-1);
     res.render('index', { title: 'Project MoNET', route : "index", latest : latest });
   });
