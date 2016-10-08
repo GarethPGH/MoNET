88c11f10ac39b50b709e61e6c3c4c0f85bb7667f
GarethPGH <garethlwarren@users.noreply.github.com>
2016-10-07 21:35:19 -0400

    Behold my Disaster!
    Submitting all the work I did on the website going in circles over the
    same damn errors over and over again. Starting from the ground up and
    going to try it again, this time with proper function names and function
    passing.

---
+++ MoNETGallery/MoNETGallery/Scripts/Thumbpager.js
@@ -0,0 +1,13 @@
+﻿window.addEventListener("hashchange", funcRef, false);
+
+//define a function that on hashchange will display next pane of 12 thumbs. or prev pane of 12 thumbs
+//check if the 12 thumbs are the 12 on initial pageload. If so disable prev button.
+//check if last thumb, disable next button
+if (thumbs.isloaded()) {
+//show page
+};
+//Load all thumbnails in the page
+//use target html to navigate through in sets of 12.
+
+//for optimization, pull first 12 and second 12, but hold off in async await for anything more than the first 2 pg of 12.
+//as each thumb is just the full image shrunk down.
---
+++ MoNETGallery/Scripts/createServer.js
@@ -0,0 +1,23 @@
+﻿fs = require(['fs']);
+http = require(['http']);
+url = require(['url']);
+console.log("thisisme");
+
+http.Server(function (req, res) {
+    var request = url.parse(req.url, true);
+    var action = request.pathname;
+    var thumbnails = getElementsByClassName("thumbnails");
+    for (var i = 0; i < thumbnails.length; i++) {
+        if (action == "click") {
+            var image = thumbnails.nextChild;
+            var img = fs.readFileSync(image);
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            res.end(img, 'binary');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('not image');
+        }
+    }
+}).listen(62519, '127.0.0.1');
+
+//this didnt work and apparently npm install does not work well with windows... 
