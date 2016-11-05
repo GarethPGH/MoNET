fd7e4f5ed6f6fbcfe0357c7cbf10daf57655970e
GarethPGH <garethlwarren@users.noreply.github.com>
2016-11-04 23:23:18 -0400

    Progress
    I got the gallery images to load in the viewer div. However, I still
    need to create a place to house the images from the robot and have it
    pull images from that location. Also must create thumbnails instead of
    loading fullsize images into smaller boxes (or use compression?) Also
    still need to allow for multiple pages of thumbnails. Probably going to
    use Bootstrap for that if I can get it to look right and use my assets.
    Also the gallery is currently populated with asset images. I am only
    using my own creations at this point and MoNET assets from main page.
    Will be changing these out with pictures from the actual project soon.

--- MoNETGallery/Scripts/LoadThumbnails.js
+++ MoNETGallery/Scripts/LoadThumbnails.js
@@ -1,35 +1,28 @@
-﻿
-    //create a function to load images into the tabs
-    var imgs = ["attractivearrow.png", "attractivearrow2.png", "attractivearrowr.png", "attractivearrowr2.png", "bird.png", "bird2.png", "birdwithhat.png", "birdwithmonocle.png", "birdwithmonocle2.png", "gallerytitle.png", "gallerytitle2.png", "gallerytitlebanner.png", "gallerytitlebanner2.png", "hero_logo_headsmall.png", "hero_logo_headsmall2.png", "hero_logo_words.png", "labels.png", "sillyraccoon.png", "sillyraccoon2.png", "yellowfrog.png", "yellowfrog2.png"];
+﻿"use strict";
+
+
+var imgs = ["attractivearrow.png", "attractivearrow2.png", "attractivearrowr.png", "attractivearrowr2.png", "bird.png", "bird2.png", "birdwithhat.png", "birdwithmonocle.png", "birdwithmonocle2.png", "gallerytitle.png", "gallerytitle2.png", "gallerytitlebanner.png", "gallerytitlebanner2.png", "hero_logo_headsmall.png", "hero_logo_headsmall2.png", "hero_logo_words.png", "labels.png", "sillyraccoon.png", "sillyraccoon2.png", "yellowfrog.png", "yellowfrog2.png"];
+
     function loadMyFnThumbnails() {
-        //add another loop to add event listener "click" that will set #Frame to image src.
+    
         var thumbs = document.getElementsByClassName("thumbnails");
+        for (var i = thumbs.length - 1; i >= 0; --i) {
+            thumbs[i].addEventListener("click", changeImage);
+        }
+       
 
-        function changeImage(btnImg) {
-            var frameImg = document.getElementById('Painting');
-            //why are you null?
-            console.log("The picture frame" + frameImg);
-            //returns HTMLImageElement
-            frameImg.src = btnImg.src;
-            //why are you undefined?
-            console.log("The picture frame" + frameImg);
-
-        };
-
-        //Getting closer but still not there yet
-
-        //create function name for function below and call it properly. It might work better this way.
-
-        for (var index = 0; index <= thumbs.length; index++) {
-
-            thumbs[index].addEventListener("click", changeImage);
+        for (var index = thumbs.length - 1; index >= 0; --index) {
+            
 
             if (thumbs[index].children.length > 0) {
-                var thumbnailImg = thumbs[index].firstElementChild;
-                thumbnailImg.setAttribute("src", "/public/images/" + imgs[index]);
-                console.log(thumbnailImg);
+
+                var thumbnailImg = thumbs[index];
+                thumbnailImg.firstElementChild.setAttribute("src", "/public/images/" + imgs[index]);
+                
+              
             };
-            //It works, but why is thumbs[i] still undefined?    
+            
         };
+      
     };
-    window.onload = loadMyFnThumbnails;
+
---
+++ MoNETGallery/Scripts/changeImage.js
@@ -0,0 +1,14 @@
+﻿//still doesnt work.
+console.log("changeimage loaded");
+
+function changeImage() {
+    var frameImg = document.getElementById('Painting');
+    //why are you null?
+    
+    //returns HTMLImageElement
+    var newImg = this;
+
+    frameImg.src = newImg.firstElementChild.src;
+    //why are you undefined?
+ 
+};
